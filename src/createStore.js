8069b3436fae62f5765ae97c4d9c10a6d536a33e
Joe Ganobsik <jganobsik@gmail.com>
2019-06-17 23:44:39 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,44 @@
 export default function createStore(reducer) {
-  // add your code here
+  function createStore() {
+    let state;
+   
+    function dispatch(action) {
+      state = changeCount(state, action);
+      render();
+    }
+   
+    function getState() {
+      return state;
+    }
+   
+    return {
+      dispatch,
+      getState
+    };
+  };
+   
+  function changeCount(state = { count: 0 }, action) {
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 };
+   
+      default:
+        return state;
+    }
+  };
+   
+  function render() {
+    let container = document.getElementById('container');
+    container.textContent = store.getState().count;
+  };
+   
+  let store = createStore();
+  store.dispatch({ type: '@@INIT' });
+  let button = document.getElementById('button');
+   
+  button.addEventListener('click', function() {
+      store.dispatch({ type: 'INCREASE_COUNT' });
+  })
 }
 
 function render() {
