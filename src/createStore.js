5fb2649a846885071337042304047b7192df7183
Adam Sarli <adam.sarli@gmail.com>
2019-10-16 16:57:43 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,17 +1,16 @@
-import candyReducer from './reducers/candyReducer'
-import countReducer from './reducers/countReducer'
-
 export default function createStore(reducer) {
   let state
 
   function dispatch(action){
     state = reducer(state, action)
-    //render()
+    render()
   }
 
   function getState() {
     return state
   }
+
+  dispatch({type: '@@INIT'})
 
   return {dispatch, getState}
 }
@@ -19,9 +18,3 @@ export default function createStore(reducer) {
 function render() {
   const container = document.getElementById('container');
 }
-
-let candyStore = createStore(candyReducer)
-//candyStore.dispatch({type: "@@INIT"})
-//
-let countStore = createStore(countReducer)
-//countStore.dispatch({type: '@@INIT'})
--- test/index-test.js
+++ test/index-test.js
@@ -17,5 +17,29 @@ describe('createStore', function() {
     expect(candyStore).toBeA(Object)
   })
 
-  
+  describe('getState', function(){
+    it('returns the default state based on the reducer for the candyStore', function(){
+      expect(candyStore.getState()).toEqual([]);
+    })
+
+    it('returns the default state based on the reducer for the countStore', function(){
+      expect(countStore.getState()).toBeA('number')
+      expect(countStore.getState()).toEqual(0)
+    })
+  })
+
+  describe('dispatch', function(){
+
+    it('can dispatch actions to update the state', function(){
+      countStore.dispatch({type: 'INCREMENT_COUNT'});
+      expect(countStore.getState()).toEqual(1);
+
+    })
+
+    it('can dispatch actions with data associated', function(){
+      candyStore.dispatch({type: 'ADD_CANDY', candy: {name: "Jelly Beans"}});
+      let todos = candyStore.getState();
+      expect(todos).toEqual([{name: "Jelly Beans"}]);
+    })
+  })
 })
