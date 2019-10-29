e8fb6763d4efe10e3655527b8460aea83f5830c6
Alan Krajina <alankrajina88@gmail.com>
2019-10-29 17:23:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // write your createStore function here
 
+function createStore(candyReducer){
+  let state;
+
+  function dispatch(action) {            // updates and creates new state
+    state = candyReducer(state, action);   // ----> OVDJE SE REDUCER POKRECE i returna i sejva novo state u state
+    render();                         // ----> spaja sa HTML,mjenja text HTMLa
+  }
+
+  function getState() {              // returns current state
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+}
+
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -20,3 +39,9 @@ function render() {
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+
+
+let store = createStore(candyReducer) // createStore takes the reducer as an argument
+
+
+store.dispatch({ type: '@@INIT' });
