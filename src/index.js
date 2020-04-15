57bd58707f13af9fc5e9eba629975a6a4ef52923
freddysilber <freddy.silber@cloudcoach.com>
2020-04-15 12:21:00 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-// write your createStore function here
+function createStore(candyReducer) {
+  let state
+
+  function dispatch(action) {
+    state = candyReducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
 
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
-      return [...state, action.candy];
+      return [...state, action.candy]
     default:
-      return state;
+      return state
   }
 }
 
 function render() {
-  let container = document.getElementById('container');
-  if(store.getState()) {
+  let container = document.getElementById('container')
+  if (store.getState()) {
     container.textContent = store.getState().join(' ')
   } else {
     throw new Error("the store's state has not been defined yet")
   }
-};
+}
+
+let store = createStore(candyReducer)
+
+store.dispatch({ type: '@@INIT' })
+
+store.dispatch({ type: 'ADD_CANDY', candy: 'cake' })
+store.dispatch({ type: 'ADD_CANDY', candy: 'chocolate' })
+store.dispatch({ type: 'ADD_CANDY', candy: 'cookies' })
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
