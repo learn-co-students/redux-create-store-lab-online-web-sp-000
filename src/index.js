7eb504e305a96d9e837c2d40e5283a30d54cd8cd
mbade1 <mbade314@gmail.com>
2020-09-10 09:39:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -20,3 +20,23 @@ function render() {
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+
+function createStore(candyReducer) {
+  let state;
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    render();
+  }
+  function getState() {
+    return state;
+  }
+
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+let store = createStore(candyReducer);
+store.dispatch({ type: '@@INIT'}) 
