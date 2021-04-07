5dae4031b4417f4885867c97f8312b59a1ec735b
anpham148 <anpham148@gmail.com>
2021-04-07 17:24:53 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function render() {
   }
 };
 
-
+store.dispatch({type: '@@INIT'})
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
