a79baf67aeea1e88bf3d8e8fd51b6f4525290adc
yarvinh <yarvinhhernandez@gmail.com>
2021-06-20 20:25:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 // write your createStore function here
+function createStore(candyReducer) {
+  let state;
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    render();
+  }
+  function getState() {
+    return state;
+  }
+  
+  return {
+    dispatch,
+    getState
+  };
+
+}
+
+
+
+
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -8,6 +28,8 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
+let store = createStore(candyReducer);
+store.dispatch({ type: '@@INIT' });
 
 function render() {
   let container = document.getElementById('container');
